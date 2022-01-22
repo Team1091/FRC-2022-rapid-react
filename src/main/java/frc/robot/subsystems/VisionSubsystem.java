33614f27de968f1460a373cecf3768e67aa6b038
Team1091 <frcteam1091@gmail.com>
2022-01-22 09:17:08 -0600

    Vision stuff

---
+++ src/main/java/frc/robot/subsystems/VisionSubsystem.java
@@ -0,0 +1,12 @@
+package frc.robot.subsystems;
+
+import frc.robot.vision.BallPosition;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class VisionSubsystem {
+    public List<BallPosition> getBallLocation() {
+        return new ArrayList<BallPosition>();
+    }
+}
