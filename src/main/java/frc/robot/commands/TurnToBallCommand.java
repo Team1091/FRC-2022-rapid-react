cc909c99928864a1ba0187dc673add49fb86f708
Team1091 <frcteam1091@gmail.com>
2022-01-22 11:05:54 -0600

    Finished the TurnToBallCommand.

---
+++ src/main/java/frc/robot/commands/TurnToBallCommand.java
@@ -0,0 +1,60 @@
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.Constants;
+import frc.robot.subsystems.DriveTrainSubsystem;
+import frc.robot.subsystems.VisionSubsystem;
+import org.opencv.core.Point;
+
+import java.util.Comparator;
+
+
+public class TurnToBallCommand extends CommandBase {
+    private final VisionSubsystem visionSubsystem;
+    private final DriveTrainSubsystem driveTrainSubsystem;
+    private Point lastSeenPosition = null;
+    private final int tolarence = 10;
+    public TurnToBallCommand(VisionSubsystem visionSubsystem, DriveTrainSubsystem driveTrainSubsystem, int tolarence){
+        this.visionSubsystem = visionSubsystem;
+        this.driveTrainSubsystem = driveTrainSubsystem;
+        addRequirements(this.driveTrainSubsystem);
+        addRequirements(this.visionSubsystem);
+    }
+
+
+    @Override
+    public void initialize() {
+
+    }
+
+    @Override
+    public void execute() {
+        var ballLocations = visionSubsystem.getBallLocation();
+        var closestBall = ballLocations.stream().max(Comparator.comparing(it->it.y)).get();
+        lastSeenPosition = closestBall;
+       if (closestBall==null){
+           driveTrainSubsystem.mecanumDrive(0,0,0.3);
+           return;
+       }
+
+       if (closestBall.x> Constants.Vision.resizeImageWidth/2){
+           driveTrainSubsystem.mecanumDrive(0,0,0.4);
+
+       } else{
+           driveTrainSubsystem.mecanumDrive(0,0,-0.4);
+       }
+
+    }
+
+    @Override
+    public void end(boolean interrupted) {
+
+    }
+
+    @Override
+    public boolean isFinished() {
+        return lastSeenPosition!=null&&Math.abs(lastSeenPosition.x-Constants.Vision.resizeImageWidth/2)<tolarence;
+
+    }
+}
+
