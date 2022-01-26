e9d85b698834706bd5b138e849231d2341579cae
Team1091 <frcteam1091@gmail.com>
2022-01-25 18:40:18 -0600

    Worked on autonomus mode

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -93,8 +93,12 @@ public class RobotContainer {
                         new TimerCommand(5)
                 ),
                 new DistanceDriveCommand(driveTrainSubsystem, 3.0),
-                new TurnToBallCommand(visionSubsystem, driveTrainSubsystem)
-
+                new TurnToBallCommand(visionSubsystem, driveTrainSubsystem),
+                new DriveUntilCanPickUpBallCommand(visionSubsystem, driveTrainSubsystem),
+                new ParallelRaceGroup(
+                    new PickUpBallCommand(ballConsumptionSubsystem),
+                    new TimerCommand(5)
+                )
         );
     }
 
